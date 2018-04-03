3150273bf0be95ed8ae941faeb31e605d805e15a
Tim Ollerton <timolly2202@hotmail.com>
2018-04-03 10:37:25 +1000

    Description adding
    Added Descriptions for the different cards in chance and community chest

---
+++ ChanceLibrary.py
@@ -0,0 +1,20 @@
+#indexing assistance
+description = 0
+
+#Library
+a = ["Advance to Go (Collect $200)"]
+b = ["Advance to Trafalgar Sq"]
+c = ["Advance to Paul Mall"]
+d = ["Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown."]
+e = ["Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank."]
+f = ["Bank pays you dividend of $50"]
+g = ["Get out of Jail Free, It will be kept until needed"]
+h = ["Go Back 3 Spaces"]
+i = ["Go to Jail – Go directly to Jail – Do not pass Go, do not collect $200"]
+j = ["Make general repairs on all your property – For each house pay $25 – For each hotel $100"]
+k = ["Pay poor tax of $15"]
+l = ["Take a trip to King's Cross Stn – If you pass Go, collect $200"]
+m = ["Take a trip to Mayfair – Advance token to Mayfair."]
+n = ["You have been elected Chairman of the Board – Pay each player $50"]
+o = ["Your building loan matures – Collect $150 "]
+p = ["You have won a crossword competition - collect $100"]
---
+++ CmuChestLibrary.py
@@ -0,0 +1,21 @@
+#indexing assistance
+description = 0
+
+#Library
+a = ["Advance to Go (Collect $200)"]
+b = ["Bank error in your favor – collect $75 "]
+c = ["Doctor's fees – Pay $50 "]
+d = ["Get out of Jail Free, It will be kept until needed"]
+e = ["Go to jail – go directly to jail – Do not pass Go, do not collect $200"]
+f = ["It is your birthday Collect $10 from each player"]
+g = ["Grand Opera Night – collect $50 from every player for opening night seats"]
+h = ["Income Tax refund – collect $20"]
+i = ["Life Insurance Matures – collect $100"]
+j = ["Pay Hospital Fees of $100"]
+k = ["Pay School Fees of $50"]
+l = ["Receive $25 Consultancy Fee"]
+m = ["You are assessed for street repairs – $40 per house, $115 per hotel"]
+n = ["You have won second prize in a beauty contest– collect $10"]
+o = ["You inherit $100"]
+p = ["From sale of stock you get $50"]
+q = ["Holiday Fund matures - Receive $100"]
