ae0f85d351a78360afe9fd3cb54837c41ff62712
Lachlan <Leafox22@gmail.com>
2018-09-07 08:38:54 +1000

    Added comments :)

--- NewPropData.py
+++ NewPropData.py
@@ -13,6 +13,7 @@ class PropertyCard(object):
         self.buildprice = buildprice
         self.owner = owner
 
+# Not sure how I'll make this work yet. Ignore if you want :)
 class consumable(object):
 
     def __init__(self, choice):
@@ -29,9 +30,9 @@ with open("PropertyStats") as prop:
         line = readin.split()
         if line[0] == "p":
             gameboard[line[2].replace("_", " ")] = PropertyCard(line[2].replace("_", " "), line[3].replace("_", " "), line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11])
+        '''
         elif line[0] == "c":
             gameboard[line[2].replace("_", " ")] = consumable(line[2].replace("_", " "))
             pass
-
-
-# print(gameboard["Mayfair"].colour)
+        '''
+# Not working yet ^
